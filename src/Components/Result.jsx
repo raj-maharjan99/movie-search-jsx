47a0418a4c1b932f463499c25634a8798780b0ea
=raj-maharjan99 <=rjuallone@gmail.com>
2023-12-24 15:19:07 -0600

    create box component and install taiwind

---
+++ src/Components/Result.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Result(props) {
+  const boxes = props.movies.map(() => {
+    return <Box />;
+  });
+  return <div className="w-full grid grid-cols-4 gap-3 ">{boxes}</div>;
+}
+
+const Box = () => {
+  return (
+    <div className="shadow min-h-[200px] border border-black mt-3">
+      <img
+        className="w-ful"
+        src="https://i5.walmartimages.com/asr/9032725b-a21f-41ff-9a7b-ab9d39c86fc2.7b2569ebad4724826b7b71915becd10f.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF"
+        alt=""
+      />
+    </div>
+  );
+};
--- src/raj.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function raj() {
-  return <div>raj</div>;
-}
-
-export default raj;
